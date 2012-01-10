15560fe6283b75a285cdc3e125820b359940e382
Jorge Aranda <jarandag@gmail.com>
2012-01-09 21:23:14 -0800

    Extremely bare-bones artificial agents join in on a single ocean simulation now.
    Closes #14.

--- ocean.js
+++ ocean.js
@@ -18,14 +18,49 @@ function handler (req, res) {
 }
 
 var players = 0;
+var expectedPlayers = 4;
+var aiPlayers = 3;
+
+function aiAgent (name) {
+	this.name = name;
+	this.greediness = 0.5;
+}
+
+function humanAgent (name) {
+	this.name = name;
+	this.greediness = null;
+}
+
+var gs = { expectedPlayers : 4,
+           expectedHumans : 1,
+           actualPlayers : 0,
+           actualHumans : 0 };
+
+var agents = new Array();
+
+for (i = 0; i < gs.expectedPlayers - gs.expectedHumans; i++) {
+	agents[i] = new aiAgent(i);
+	gs.actualPlayers++;
+}
 
 io.sockets.on('connection', function (socket) {
-	players = ++players;
-	myNumber = players;
-	gs = { player : myNumber, test : "passed" };
+	agents[gs.actualPlayers] = new humanAgent(gs.actualPlayers);
+	var myID = gs.actualPlayers++;
+	gs.actualHumans++;
 	
 	socket.set('gamesettings', gs, function() {
 		socket.emit('gamesettings', gs);
-	
 	});
+	
+	socket.set('myID', myID, function() {
+		socket.emit('myID', myID);
+	});
+	
+	socket.set('agents', agents, function() {
+		socket.emit('agents', agents);
+	});
+	
+	if (gs.actualPlayers == gs.expectedPlayers) {
+		io.sockets.emit('news', 'All agents connected!');
+	}
 });
