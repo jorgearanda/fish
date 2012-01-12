f87285ff47a7d496484ac1b359832f7238136499
Jorge Aranda <jarandag@gmail.com>
2012-01-11 20:43:36 -0800

    Fleshing out information in main client window.
    Closes #16.
    Still need to make table at bottom pretty; also, there could be more
    information coming in, or information in a different format. But I'll
    tackle that eventually.

--- ocean.js
+++ ocean.js
@@ -24,13 +24,20 @@ var aiPlayers = 3;
 function aiAgent (name) {
 	this.name = name;
 	this.greediness = 0.5;
+	this.fishCaught = 0;
+	this.money = 100;
+	this.status = 'At port';
 }
 
 function humanAgent (name) {
 	this.name = name;
 	this.greediness = null;
+	this.fishCaught = 0;
+	this.money = 100;
+	this.status = 'At port';
 }
 
+var agents = new Array();
 var gs = { expectedPlayers : 4,
            expectedHumans : 2,
            actualPlayers : 0,
@@ -39,12 +46,19 @@ var gs = { expectedPlayers : 4,
            totalSeasons : 4,
            currentSeason : 0,
            seasonDuration : 60,
+           certainFish : 10,
+           mysteryFish : 0,
+           actualMysteryFish : 0,
+           costDepart : 10,
+           costCast : 2,
+           costAtSea : 1,
+           valueFish : 5,
+           players : agents,
            status : 'waiting' };
 
-var agents = new Array();
 
 for (i = 0; i < gs.expectedPlayers - gs.expectedHumans; i++) {
-	agents[i] = new aiAgent(i);
+	agents[i] = new aiAgent('Robot ' + i);
 	gs.actualPlayers++;
 }
 
@@ -67,6 +81,7 @@ io.sockets.on('connection', function (socket) {
 	
 	if (gs.actualPlayers == gs.expectedPlayers) {
 		gs.status = 'running';
+		gs.currentSeason = 1;
 		io.sockets.emit('gamesettings', gs);
 		io.sockets.emit('begin', 'All agents connected!');
 	}
