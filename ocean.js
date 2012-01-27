5bee93e2dc1af199182295bff355b2241a0e7a93
Jorge Aranda <jarandag@gmail.com>
2012-01-26 16:30:27 -0800

    Add functionality for artificial agents to leave port, fish, and return.
    Closes #19.
    Still need to introduce variability to AI behaviour, and to space out
    AI's actions more.

--- ocean.js
+++ ocean.js
@@ -20,17 +20,24 @@ function handler (req, res) {
 var players = 0;
 var expectedPlayers = 4;
 var aiPlayers = 3;
+var startingFish = 10;
+var chanceOfCatch = 1.0;
+var spawnFactor = 4.0;
 
 function aiAgent (name) {
 	this.name = name;
+	this.type = 'ai';
 	this.greediness = 0.5;
 	this.fishCaught = 0;
 	this.money = 100;
 	this.status = 'At port';
+	this.intendedCasts = Math.round(((startingFish - (startingFish / spawnFactor)) / expectedPlayers) * 2 * this.greediness / chanceOfCatch);
+	this.actualCasts = 0;
 }
 
 function humanAgent (name) {
 	this.name = name;
+	this.type = 'human';
 	this.greediness = null;
 	this.fishCaught = 0;
 	this.money = 100;
@@ -53,8 +60,11 @@ var gs = { expectedPlayers : 4,
            costCast : 2,
            costAtSea : 1,
            valueFish : 5,
+           chanceOfCatch : 1.00,
+           spawnFactor : 4.0,
            players : agents,
            status : 'waiting' };
+
 
 
 for (i = 0; i < gs.expectedPlayers - gs.expectedHumans; i++) {
@@ -85,7 +95,7 @@ io.sockets.on('connection', function (socket) {
 		io.sockets.emit('gamesettings', gs);
 		io.sockets.emit('begin', 'All agents connected!');
 	}
-	
+
 	socket.on('toSea', function (data) {
 		console.log("A player sailed to sea: " + data.id);
 		gs.players[data.id].status = 'At sea';
@@ -103,5 +113,45 @@ io.sockets.on('connection', function (socket) {
 		}
 		io.sockets.emit('gamesettings', gs);
 	});
-	
+
+	// Begin timekeeping
+	timer();
 });
+
+var secondsSinceStart = 0;
+var t;
+function timer() {
+	if (gs.actualPlayers == gs.expectedPlayers) {
+		secondsSinceStart += 1;
+		console.log('Seconds since start of season: ' + secondsSinceStart);
+		io.sockets.emit('time', secondsSinceStart);
+		
+		for (i = 0; i < gs.players.length; i++) {
+			if (gs.players[i].type == 'ai') {
+				aiActions(i);
+			}
+		}
+	} else {
+		console.log('Waiting for players.');
+	}
+	t = setTimeout(timer, 1000);
+}
+
+function aiActions(ag) {
+	if ((gs.players[ag].intendedCasts > gs.players[ag].actualCasts) && 	(gs.players[ag].status == 'At port') &&
+	(gs.certainFish + gs.actualMysteryFish > 0)) {
+		console.log("A player sailed to sea: " + ag);
+		gs.players[ag].status = 'At sea';
+		gs.players[ag].money -= gs.costDepart;
+	} else if ((gs.players[ag].intendedCasts > gs.players[ag].actualCasts) && 	(gs.players[ag].status == 'At sea') &&
+	(gs.certainFish + gs.actualMysteryFish > 0)) {
+		console.log("A player tried to fish: " + ag);
+		gs.players[ag].money -= gs.costCast;
+		gs.players[ag].money += gs.valueFish;
+		gs.players[ag].actualCasts ++;
+		gs.certainFish -= 1;
+	} else if (((gs.players[ag].intendedCasts <= gs.players[ag].actualCasts) || (gs.certainFish + gs.actualMysteryFish <= 0)) && gs.players[ag].status == 'At sea') {
+		gs.players[ag].status = 'At port';
+	}
+	io.sockets.emit('gamesettings', gs);
+}
