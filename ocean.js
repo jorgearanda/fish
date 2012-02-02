1a60804e0b7c970d13fa5ef2952bd7c2fb72dee1
Jorge Aranda <jarandag@gmail.com>
2012-02-02 11:34:55 -0800

    Big update: restructured ocean.js to support synchronous simulations.
    All of the simulations are now held under the games array, and the
    timer() function checks them all to see if there's anything to do in
    them.
    
    Remaining to do, but not essential, is to remove games from the array
    once the simulation has ended and we've logged all its relevant
    information.
    
    Closes #23.

--- ocean.js
+++ ocean.js
@@ -12,6 +12,100 @@ function engine(io) {
 	var chanceOfCatch = 1.0;
 	var spawnFactor = 4.0;
 
+	var t;
+	var beginTimer = false;
+	var endTimer = false;
+
+	function timer() {
+		if (!endTimer) {
+			for (game in games) {
+				if (games[game].timable == true) {
+					timeStep(game);
+				}
+			}
+			t = setTimeout(timer, 1000);
+		} else {
+			console.log('Finishing timer---we are done.');
+			beginTimer = false;
+			endTimer = true;
+		}
+	}
+
+	function timeStep(gameName) {
+		g = games[gameName];
+		if (g.actualPlayers == g.expectedPlayers) {
+			if (g.status == 'running') {
+				g.currentSeconds += 1;
+				if (g.seasonDuration <= g.currentSeconds) {
+					g.status = 'paused';
+					g.currentSeconds = 0;
+					for (i = 0; i < g.players.length; i++) {
+						g.players[i].status = 'At port';
+						g.players[i].actualCasts = 0;
+					}
+					if (g.currentSeason < g.totalSeasons) {
+						console.log('Season ended on gameroom ' + gameName + ', beginning pause period.');
+						io.sockets.in(gameName).emit('gamesettings', g);
+						io.sockets.in(gameName).emit('endseason', g.currentSeason);
+					} else {
+						g.status = 'over';
+						console.log('Simulation ended for gameroom ' + gameName);
+						io.sockets.in(gameName).emit('gamesettings', g);
+						io.sockets.in(gameName).emit('gameover', 'gameover');
+						g.timable = false;
+					}
+				} else {
+					console.log('Seconds in season for gameroom ' + gameName + ': ' + g.currentSeconds);
+					io.sockets.in(gameName).emit('time', g.currentSeconds);
+					for (i = 0; i < g.players.length; i++) {
+						if (g.players[i].type == 'ai') {
+							aiActions(g, gameName, i);
+						}
+						if (g.players[i].status == 'At sea') {
+							g.players[i].money -= g.costAtSea;
+						}
+					}
+					io.sockets.in(gameName).emit('gamesettings', g);
+				}
+			} else {
+				g.currentSeconds += 1;
+				if (g.pauseDuration <= g.currentSeconds) {
+					g.certainFish *= g.spawnFactor;
+					g.actualMysteryFish *= g.spawnFactor;
+					g.status = 'running';
+					g.currentSeconds = 0;
+					g.currentSeason += 1;
+					console.log('Beginning new season in gameroom ' + gameName);
+					io.sockets.in(gameName).emit('begin', 'New season');
+				} else {
+					console.log('Seconds since pausing gameroom ' + gameName + ': ' + g.currentSeconds);
+					io.sockets.in(gameName).emit('pausetime', g.currentSeconds);
+				}
+			}
+		} else {
+			console.log('Waiting for players in gameroom ' + gameName);
+		}
+	}
+
+	function aiActions(g, gameName, agentID) {
+		agent = g.players[agentID];
+		if ((agent.intendedCasts > agent.actualCasts) && 	(agent.status == 'At port') && (g.certainFish + g.actualMysteryFish > 0)) {
+			console.log("A player sailed to sea: " + agent.name + ", gameroom " + gameName + ".");
+			agent.status = 'At sea';
+			agent.money -= g.costDepart;
+		} else if ((agent.intendedCasts > agent.actualCasts) && 	(agent.status == 'At sea') && (g.certainFish + g.actualMysteryFish > 0)) {
+			console.log("A player tried to fish: " + agent.name + ", gameroom " + gameName + ".");
+			agent.money -= g.costCast;
+			agent.money += g.valueFish;
+			agent.actualCasts ++;
+			g.certainFish -= 1;
+			agent.fishCaught++;
+		} else if (((agent.intendedCasts <= agent.actualCasts) || (g.certainFish + g.actualMysteryFish <= 0)) && agent.status == 'At sea') {
+			agent.status = 'At port';
+		}
+		io.sockets.in(gameName).emit('gamesettings', g);
+	}
+		
 	function aiAgent (name) {
 		this.name = name;
 		this.type = 'ai';
@@ -38,11 +132,11 @@ function engine(io) {
 		this.expectedHumans = 2;
 		this.actualPlayers = 0;
 		this.actualHumans = 0;
-		this.mode = "standby";
+		this.timable = true;
 		this.totalSeasons = 4;
 		this.currentSeason = 0;
-		this.seasonDuration = 0;
-		this.pauseDuration = 10;
+		this.seasonDuration = 10;
+		this.pauseDuration = 5;
 		this.certainFish = 40;
 		this.mysteryFish = 0;
 		this.actualMysteryFish = 0;
@@ -54,6 +148,7 @@ function engine(io) {
 		this.spawnFactor = 4.00;
 		this.players = new Array();
 		this.status = "waiting";
+		this.currentSeconds = 0;
 		
 		for (i = 0; i < this.expectedPlayers - this.expectedHumans; i++) {
 			this.players[i] = new aiAgent('Robot ' + i);
@@ -61,7 +156,6 @@ function engine(io) {
 		}
 	}
 
-	var beginTimer = false;
 	io.sockets.on('connection', function (socket) {
 		var myID;
 		socket.on('join group', function (group) {
@@ -83,152 +177,54 @@ function engine(io) {
 			games[group].actualHumans++;
 			io.sockets.in(group).emit("gamesettings", games[group]);
 
-		socket.set('gamesettings', games[group], function() {
-			socket.emit('gamesettings', games[group]);
-		});
+			socket.set('gamesettings', games[group], function() {
+				socket.emit('gamesettings', games[group]);
+			});
 	
-		socket.set('myID', myID, function() {
-			socket.emit('myID', myID);
-		});
+			socket.set('myID', myID, function() {
+				socket.emit('myID', myID);
+			});
 	
-		//socket.set('agents', players, function() {
-		//	socket.emit('agents', games[group].players);
-		//});
-	
-		if (games[group].actualPlayers == games[group].expectedPlayers) {
-			games[group].status = 'running';
-			games[group].currentSeason = 1;
-			io.sockets.in(group).emit('gamesettings', games[group]);
-			io.sockets.in(group).emit('begin', 'All agents connected!');
-		}
-		
-		socket.on('toSea', function (data) {
-			console.log("A player sailed to sea: " + data.id);
-			games[group].players[data.id].status = 'At sea';
-			games[group].players[data.id].money -= games[group].costDepart;
-			io.sockets.in(group).emit('gamesettings', games[group]);
-		});
-
-		socket.on('toPort', function (data) {
-			console.log("A player returned to port: " + data.id);
-			games[group].players[data.id].status = 'At port';
-			io.sockets.in(group).emit('gamesettings', games[group]);
-		});
-
-		socket.on('fishing', function (data) {
-			console.log("A player tried to fish: " + data.id);
-			games[group].players[data.id].money -= games[group].costCast;
-			games[group].players[data.id].actualCasts++;
-			if (games[group].certainFish + games[group].actualMysteryFish > 0) {
-				games[group].players[data.id].money += games[group].valueFish;
-				games[group].players[data.id].fishCaught++;
-				// Right now we're only removing actual fish, not mystery fish...
-				games[group].certainFish -= 1;
+			if (games[group].actualPlayers == games[group].expectedPlayers) {
+				games[group].status = 'running';
+				games[group].currentSeason = 1;
+				io.sockets.in(group).emit('gamesettings', games[group]);
+				io.sockets.in(group).emit('begin', 'All agents connected!');
 			}
-			io.sockets.in(group).emit('gamesettings', games[group]);
-		});
+		
+			socket.on('toSea', function (data) {
+				console.log("A player sailed to sea: " + data.id + ", gameroom " + group + ".");
+				games[group].players[data.id].status = 'At sea';
+				games[group].players[data.id].money -= games[group].costDepart;
+				io.sockets.in(group).emit('gamesettings', games[group]);
+			});
+
+			socket.on('toPort', function (data) {
+				console.log("A player returned to port: " + data.id + ", gameroom " + group + ".");
+				games[group].players[data.id].status = 'At port';
+				io.sockets.in(group).emit('gamesettings', games[group]);
+			});
+
+			socket.on('fishing', function (data) {
+				console.log("A player tried to fish: " + data.id + ", gameroom " + group + ".");
+				games[group].players[data.id].money -= games[group].costCast;
+				games[group].players[data.id].actualCasts++;
+				if (games[group].certainFish + games[group].actualMysteryFish > 0) {
+					games[group].players[data.id].money += games[group].valueFish;
+					games[group].players[data.id].fishCaught++;
+					// Right now we're only removing actual fish, not mystery fish...
+					games[group].certainFish -= 1;
+				}
+				io.sockets.in(group).emit('gamesettings', games[group]);
+			});
 	
-		// Begin timekeeping
-		if (beginTimer == false) {
-			beginTimer = true;
-			timer();
-		}
-		
-		function timeStep() {
-		if (games[group].actualPlayers == games[group].expectedPlayers) {
-			if (games[group].status == 'running') {
-				secondsSinceStart += 1;
-				if (games[group].seasonDuration <= secondsSinceStart) {
-					games[group].status = 'paused';
-					secondsSinceStart = 0;
-					for (i = 0; i < games[group].players.length; i++) {
-						games[group].players[i].status = 'At port';
-						games[group].players[i].actualCasts = 0;
-					}
-					if (games[group].currentSeason < games[group].totalSeasons) {
-						console.log('Season ended, beginning pause period.');
-						io.sockets.in(group).emit('gamesettings', games[group]);
-						io.sockets.in(group).emit('endseason', games[group].currentSeason);
-					} else {
-						games[group].status = 'over';
-						console.log('Simulation ended.');
-						io.sockets.in(group).emit('gamesettings', games[group]);
-						io.sockets.in(group).emit('gameover', 'gameover');
-						endTimer = true;
-					}
-				} else {
-					console.log('Seconds since start of season: ' + secondsSinceStart);
-					io.sockets.in(group).emit('time', secondsSinceStart);
-					for (i = 0; i < games[group].players.length; i++) {
-						if (games[group].players[i].type == 'ai') {
-							aiActions(i, gs, group);
-						}
-						if (games[group].players[i].status == 'At sea') {
-							games[group].players[i].money -= games[group].costAtSea;
-						}
-					}
-					io.sockets.in(group).emit('gamesettings', games[group]);
-				}
-			} else {
-				secondsSinceStart += 1;
-				if (games[group].pauseDuration <= secondsSinceStart) {
-					games[group].certainFish *= games[group].spawnFactor;
-					games[group].actualMysteryFish *= games[group].spawnFactor;
-					games[group].status = 'running';
-					secondsSinceStart = 0;
-					games[group].currentSeason += 1;
-					console.log('Beginning new season.');
-					io.sockets.in(group).emit('begin', 'New season');
-				} else {
-					console.log('Seconds since pausing: ' + secondsSinceStart);
-					io.sockets.in(group).emit('pausetime', secondsSinceStart);
-				}
+			// Begin timekeeping
+			if (beginTimer == false) {
+				beginTimer = true;
+				timer();
 			}
-		} else {
-			console.log('Waiting for players.');
-		}
-		
-		}
-
-	function aiActions(ag) {
-		if ((games[group].players[ag].intendedCasts > games[group].players[ag].actualCasts) && 	(games[group].players[ag].status == 'At port') &&
-		(games[group].certainFish + games[group].actualMysteryFish > 0)) {
-			console.log("A player sailed to sea: " + ag);
-			games[group].players[ag].status = 'At sea';
-			games[group].players[ag].money -= games[group].costDepart;
-		} else if ((games[group].players[ag].intendedCasts > games[group].players[ag].actualCasts) && 	(games[group].players[ag].status == 'At sea') &&
-		(games[group].certainFish + games[group].actualMysteryFish > 0)) {
-			console.log("A player tried to fish: " + ag);
-			games[group].players[ag].money -= games[group].costCast;
-			games[group].players[ag].money += games[group].valueFish;
-			games[group].players[ag].actualCasts ++;
-			games[group].certainFish -= 1;
-			games[group].players[ag].fishCaught++;
-		} else if (((games[group].players[ag].intendedCasts <= games[group].players[ag].actualCasts) || (games[group].certainFish + games[group].actualMysteryFish <= 0)) && games[group].players[ag].status == 'At sea') {
-			games[group].players[ag].status = 'At port';
-		}
-		io.sockets.in(group).emit('gamesettings', games[group]);
-	}
-		
 		});
-
 	});
-
-	var secondsSinceStart = 0;
-	var t;
-	var endTimer = false;
-
-	function timer() {
-		if (!endTimer) {
-			timeStep();
-			t = setTimeout(timer, 1000);
-		} else {
-			console.log('Over and out.');
-			beginTimer = false;
-		}
-	}
-
-
 }
 
 
