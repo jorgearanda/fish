25ce4e3a7d58e5f30745c5478c4f81e6144e629d
Jorge Aranda <jarandag@gmail.com>
2012-03-20 12:00:05 -0700

    Finished implementing the preparatory text structure
    Every player now sees the instructions when going to main.html, and the
    server waits until everyone clicks on 'Go Fishing' to begin the
    simulation.
    
    Closes #26.

--- ocean.js
+++ ocean.js
@@ -34,7 +34,18 @@ function engine(io) {
 	function timeStep(gameName) {
 		g = games[gameName];
 		if (g.actualPlayers == g.expectedPlayers) {
-			if (g.status == 'running') {
+            if (g.status == 'waiting') {
+                console.log('Waiting for all players to read the preparatory text and click on Go Fishing, in gameroom ' + gameName);
+            } else if (g.status == 'readying') {
+				g.currentSeconds += 1;
+				if (g.currentSeconds > 5) {
+					g.status = 'running';
+					g.currentSeconds = 0;
+					console.log('Beginning first season in gameroom ' + gameName);
+					io.sockets.in(gameName).emit('begin', 'Beginning first season');
+					io.sockets.in(gameName).emit('gamesettings', g);
+				}
+			} else if (g.status == 'running') {
 				g.currentSeconds += 1;
 				if (g.seasonDuration <= g.currentSeconds) {
 					g.status = 'paused';
@@ -129,6 +140,7 @@ function engine(io) {
 		this.status = 'At port';
 		this.intendedCasts = Math.round(((startingFish - (startingFish / spawnFactor)) / expectedPlayers) * 2 * this.greediness / chanceOfCatch);
 		this.actualCasts = 0;
+        this.readRules = true;
 	}
 
 	function humanAgent (name) {
@@ -143,6 +155,7 @@ function engine(io) {
 		this.endMoneyPerSeason = new Array();
 		this.status = 'At port';
 		this.actualCasts = 0;
+        this.readRules = false;
 	}
 	
 	function gameParameters () {
@@ -167,7 +180,7 @@ function engine(io) {
 		this.players = new Array();
 		this.status = "waiting";
 		this.currentSeconds = 0;
-		this.prepText = "FISH simulates fishing in an ocean. You and the other fishers are the only fishers in this ocean. All the fishers see the same ocean that you do. At the beginning, the number of fish will be displayed on the screen. However, sometimes there is some uncertainty about the number of fish. In those cases, 'mystery fish' will be shown on the screen as well, and the number is displayed as a certain range, not as an absolute number. Once the simulation begins, you and the other fishers may catch as many of these fish as you like. Once  you have taken as many fish as you want, you return to port with your catches, and the first season ends. Then the fish spawn for the next season, if any are left to spawn (if no fish are left, they cannot spawn). For every fish left at the end of one season, two fish will be available to be caught in the next season. However, because the ocean can support only so many fish, the total number of fish will never exceed the original number of fish. Fishing can go on this way for many seasons, but all fishing permanently ceases any time that all the fish are caught.";
+		this.prepText = "FISH simulates fishing in an ocean. You and the other fishers are the only fishers in this ocean. All the fishers see the same ocean that you do. At the beginning, the number of fish will be displayed on the screen. However, sometimes there is some uncertainty about the number of fish. In those cases, 'mystery fish' will be shown on the screen as well, and the number is displayed as a certain range, not as an absolute number. Once the simulation begins, you and the other fishers may catch as many of these fish as you like. Once  you have taken as many fish as you want, you return to port with your catches, and the first season ends. Then the fish spawn for the next season, if any are left to spawn (if no fish are left, they cannot spawn). For every fish left at the end of one season, two fish will be available to be caught in the next season. However, because the ocean can support only so many fish, the total number of fish will never exceed the original number of fish. Fishing can go on this way for many seasons, but all fishing permanently ceases any time that all the fish are caught.\n\nYou can make money fishing. You will be paid $5 for every fish you catch. (For now, this is 'play' money...but please treat it as if it were real money.)\n\nYour job is to consider all these factors, and the other fishers, and make your own decisions about how to fish. Fish however you wish.\n\nPlease ask if anything is unclear. We want you to fully understand the rules before you start fishing.\n\nIf you are sure you understand all the above, you are ready to fish. Click on the Go Fishing button below when you are ready. Once all the fishers have clicked this button, the first season will begin. (You may have to wait briefly for all the others fishers to click the button.)";
 		
 		for (i = 0; i < this.expectedPlayers - this.expectedHumans; i++) {
 			this.players[i] = new aiAgent('Robot ' + i);
@@ -204,17 +217,17 @@ function engine(io) {
 				socket.emit('myID', myID);
 			});
 	
-			if (games[group].actualPlayers == games[group].expectedPlayers) {
-				games[group].status = 'running';
-				games[group].currentSeason = 1;
-				for (i = 0; i < games[group].players.length; i++) {
-					games[group].players[i].startMoneyPerSeason[1] = games[group].players[i].startMoney;
-					games[group].players[i].endMoneyPerSeason[1] = games[group].players[i].startMoney;
-					games[group].players[i].fishCaughtPerSeason[1] = 0;
-				}
-				io.sockets.in(group).emit('gamesettings', games[group]);
-				io.sockets.in(group).emit('begin', 'All agents connected!');
-			}
+			//if (games[group].actualPlayers == games[group].expectedPlayers) {
+			//	games[group].status = 'readying';
+			//	games[group].currentSeason = 1;
+			//	for (i = 0; i < games[group].players.length; i++) {
+			//		games[group].players[i].startMoneyPerSeason[1] = games[group].players[i].startMoney;
+			//		games[group].players[i].endMoneyPerSeason[1] = games[group].players[i].startMoney;
+			//		games[group].players[i].fishCaughtPerSeason[1] = 0;
+			//	}
+			//	io.sockets.in(group).emit('gamesettings', games[group]);
+			//	io.sockets.in(group).emit('readying', 'All agents ready - prepare!');
+			//}
 		
 			socket.on('toSea', function (data) {
 				console.log("A player sailed to sea: " + data.id + ", gameroom " + group + ".");
@@ -229,6 +242,28 @@ function engine(io) {
 				games[group].players[data.id].status = 'At port';
 				io.sockets.in(group).emit('gamesettings', games[group]);
 			});
+                  
+                  socket.on('readRules', function(data) {
+                            console.log("A player read the rules and is ready to start: " + data.id + ", gameroom " + group + ".");
+                            games[group].players[data.id].readRules = true;
+                            allReadRules = true;
+                            for (i = 0; i < games[group].players.length; i++) {
+                                if (games[group].players[i].readRules == false) {
+                                    allReadRules = false;
+                                }
+                            }
+                            if (games[group].actualPlayers == games[group].expectedPlayers && allReadRules) {
+                                games[group].status = 'readying';
+                                games[group].currentSeason = 1;
+                                for (i = 0; i < games[group].players.length; i++) {
+                                    games[group].players[i].startMoneyPerSeason[1] = games[group].players[i].startMoney;
+                                    games[group].players[i].endMoneyPerSeason[1] = games[group].players[i].startMoney;
+                                    games[group].players[i].fishCaughtPerSeason[1] = 0;
+                                }
+                                io.sockets.in(group).emit('gamesettings', games[group]);
+                                io.sockets.in(group).emit('readying', 'All agents ready - prepare!');
+                            }
+                            });
 
 			socket.on('fishing', function (data) {
 				console.log("A player tried to fish: " + data.id + ", gameroom " + group + ".");
