acc5e863457f059bbfd44fbaee1571d56d9c111e
Jorge Aranda <jarandag@gmail.com>
2012-04-10 17:01:44 -0700

    More aesthetic changes to UI, focusing on buttons.
    I would still like buttons to slightly change colour on mouseOver, but
    for now this should be enough.
    Closes #27.

--- ocean.js
+++ ocean.js
@@ -192,8 +192,13 @@ function engine(io) {
         this.prepText = "FISH simulates fishing in an ocean. You and the other fishers are the only fishers in this ocean. All the fishers see the same ocean that you do. At the beginning, the number of fish will be displayed on the screen. However, sometimes there is some uncertainty about the number of fish. In those cases, 'mystery fish' will be shown on the screen as well, and the number is displayed as a certain range, not as an absolute number. Once the simulation begins, you and the other fishers may catch as many of these fish as you like. Once  you have taken as many fish as you want, you return to port with your catches, and the first season ends. Then the fish spawn for the next season, if any are left to spawn (if no fish are left, they cannot spawn). For every fish left at the end of one season, two fish will be available to be caught in the next season. However, because the ocean can support only so many fish, the total number of fish will never exceed the original number of fish. Fishing can go on this way for many seasons, but all fishing permanently ceases any time that all the fish are caught.\n\nYou can make money fishing. You will be paid $5 for every fish you catch. (For now, this is 'play' money...but please treat it as if it were real money.)\n\nYour job is to consider all these factors, and the other fishers, and make your own decisions about how to fish. Fish however you wish.\n\nPlease ask if anything is unclear. We want you to fully understand the rules before you start fishing.\n\nIf you are sure you understand all the above, you are ready to fish. Click on the Go Fishing button below when you are ready. Once all the fishers have clicked this button, the first season will begin. (You may have to wait briefly for all the others fishers to click the button.)";
         this.debug = true;
 
+        robotNames = new Array();
+        robotNames[0] = "Leonardo";
+        robotNames[1] = "Michelangelo";
+        robotNames[2] = "Raphael";
+        robotNames[3] = "Donatello";
         for (i = 0; i < this.expectedPlayers - this.expectedHumans; i++) {
-            this.players[i] = new aiAgent('Robot ' + i, this.expectedPlayers, this.startingFish, this.actualMysteryFish, this.spawnFactor, this.chanceOfCatch);
+            this.players[i] = new aiAgent(robotNames[i], this.expectedPlayers, this.startingFish, this.actualMysteryFish, this.spawnFactor, this.chanceOfCatch);
             this.actualPlayers++;
         }
     }
