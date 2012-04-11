8584e49ca54eddc13b458538d604d57001a4352f
Jorge Aranda <jarandag@gmail.com>
2012-04-10 20:48:46 -0700

    Some refactoring; adding newgroup page handling

--- ocean.js
+++ ocean.js
@@ -4,14 +4,6 @@ var engineCalled = false;
 function engine(io) {
     var games = new Array();
     var group;
-
-    //var players = 0;
-    //var expectedPlayers = 4;
-    //var aiPlayers = 3;
-    //var startingFish = 40;
-    //var chanceOfCatch = 1.0;
-    //var spawnFactor = 4.0;
-
     var t;
     var beginTimer = false;
     var endTimer = false;
@@ -189,7 +181,29 @@ function engine(io) {
         this.players = new Array();
         this.status = "waiting";
         this.currentSeconds = 0;
-        this.prepText = "FISH simulates fishing in an ocean. You and the other fishers are the only fishers in this ocean. All the fishers see the same ocean that you do. At the beginning, the number of fish will be displayed on the screen. However, sometimes there is some uncertainty about the number of fish. In those cases, 'mystery fish' will be shown on the screen as well, and the number is displayed as a certain range, not as an absolute number. Once the simulation begins, you and the other fishers may catch as many of these fish as you like. Once  you have taken as many fish as you want, you return to port with your catches, and the first season ends. Then the fish spawn for the next season, if any are left to spawn (if no fish are left, they cannot spawn). For every fish left at the end of one season, two fish will be available to be caught in the next season. However, because the ocean can support only so many fish, the total number of fish will never exceed the original number of fish. Fishing can go on this way for many seasons, but all fishing permanently ceases any time that all the fish are caught.\n\nYou can make money fishing. You will be paid $5 for every fish you catch. (For now, this is 'play' money...but please treat it as if it were real money.)\n\nYour job is to consider all these factors, and the other fishers, and make your own decisions about how to fish. Fish however you wish.\n\nPlease ask if anything is unclear. We want you to fully understand the rules before you start fishing.\n\nIf you are sure you understand all the above, you are ready to fish. Click on the Go Fishing button below when you are ready. Once all the fishers have clicked this button, the first season will begin. (You may have to wait briefly for all the others fishers to click the button.)";
+        this.prepText = "FISH simulates fishing in an ocean. You and the other fishers are the only fishers " +
+                        "in this ocean. All the fishers see the same ocean that you do. At the beginning, the " +
+                        "number of fish will be displayed on the screen. However, sometimes there is some " +
+                        "uncertainty about the number of fish. In those cases, 'mystery fish' will be shown on " +
+                        "the screen as well, and the number is displayed as a certain range, not as an absolute " +
+                        "number. Once the simulation begins, you and the other fishers may catch as many of these " +
+                        "fish as you like. Once  you have taken as many fish as you want, you return to port " +
+                        "with your catches, and the first season ends. Then the fish spawn for the next season, " +
+                        "if any are left to spawn (if no fish are left, they cannot spawn). For every fish left " +
+                        "at the end of one season, two fish will be available to be caught in the next season. " +
+                        "However, because the ocean can support only so many fish, the total number of fish will " +
+                        "never exceed the original number of fish. Fishing can go on this way for many seasons, " +
+                        "but all fishing permanently ceases any time that all the fish are caught.\n\n" +
+                        "You can make money fishing. You will be paid $5 for every fish you catch. (For now, " +
+                        "this is 'play' money...but please treat it as if it were real money.)\n\n" +
+                        "Your job is to consider all these factors, and the other fishers, and make your own " +
+                        "decisions about how to fish. Fish however you wish.\n\n" +
+                        "Please ask if anything is unclear. We want you to fully understand the rules before you " +
+                        "start fishing.\n\n" +
+                        "If you are sure you understand all the above, you are ready to fish. Click on the Go " +
+                        "Fishing button below when you are ready. Once all the fishers have clicked this button, " +
+                        "the first season will begin. (You may have to wait briefly for all the others fishers " +
+                        "to click the button.)";
         this.debug = true;
 
         robotNames = new Array();
@@ -329,6 +343,19 @@ function mainadmin(response, io) {
         });
 }
 
+function newgroup(response, io) {
+    console.log("Request handler 'newgroup' was called.");
+    fs.readFile(__dirname + '/newgroup.html',
+        function (err, data) {
+            if (err) {
+                response.writeHead(500);
+                return response.end('Error loading newgroup.html');
+            }
+            response.writeHead(200);
+            response.end(data);
+        });
+}
+
 function certainfish(response, io) {
     console.log("Request handler 'certainfish' was called.");
     fs.readFile(__dirname + '/certain-fish.png',
@@ -355,5 +382,6 @@ function mysteryfish(response, io) {
 
 exports.fish = fish;
 exports.mainadmin = mainadmin;
+exports.newgroup = newgroup;
 exports.certainfish = certainfish;
 exports.mysteryfish = mysteryfish;
