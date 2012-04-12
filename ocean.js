f6c6c7cc3e3559fe75dc0ec18482dee4527fe7a4
Jorge Aranda <jarandag@gmail.com>
2012-04-11 20:34:26 -0700

    Implement show/hide names of other fishers.
    Closes #34.

--- ocean.js
+++ ocean.js
@@ -186,6 +186,7 @@ function engine(io) {
             this.startingMysteryFish = gs.actualMysteryFish;
             this.actualMysteryFish = gs.actualMysteryFish;
             this.showOtherFishers = gs.showOtherFishers;
+            this.showFisherNames = gs.showFisherNames;
             this.prepText = gs.prepText;
             for (i = 0; i < this.expectedPlayers - this.expectedHumans; i++) {
                 this.players[i] = new aiAgent(gs.robots[i].name, gs.robots[i].greed, this.expectedPlayers,
@@ -211,6 +212,7 @@ function engine(io) {
             this.startingMysteryFish = 5;
             this.actualMysteryFish = 5;
             this.showOtherFishers = true;
+            this.showFisherNames = true;
             this.prepText = "FISH simulates fishing in an ocean. You and the other fishers are the only fishers " +
                 "in this ocean. All the fishers see the same ocean that you do. At the beginning, the " +
                 "number of fish will be displayed on the screen. However, sometimes there is some " +
