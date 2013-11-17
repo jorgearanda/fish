5a34b25b1796e63025958314a28a506226adf704
Jorge Aranda <jarandag@gmail.com>
2013-11-17 10:47:29 -0800

    Beginning refactor to base Fish on express

---
+++ app.js
@@ -0,0 +1,74 @@
+'use strict';
+/*jshint -W024 */
+
+var express = require('express');
+var http = require('http');
+var logger = require('winston');
+var path = require('path');
+var socketio = require('socket.io');
+
+var app = exports.app = express();
+
+
+app.configure(function() {
+   logger.cli();
+   logger.add(logger.transports.File, {
+      filename: 'fish.log',
+      handleExceptions: false
+   });
+
+   if (process.env.NODE_ENV === 'test') {
+      logger.remove(logger.transports.Console);
+   }
+
+   if (app.settings.env === 'development') {
+      process.env.NODE_ENV = 'development';
+      app.use(express.logger('dev'));
+      app.use(express.errorHandler());
+   } else if (app.settings.env === 'production') {
+      var loggerStream = {
+         write: function (message) { logger.info(message.slice(0, -1)); }
+      };
+
+      app.use(express.logger({ stream: loggerStream }));
+   }
+
+   app.set('port', process.env.PORT || 8080);
+
+   app.set('views', __dirname + '/views');
+   app.engine('html', require('ejs').renderFile);
+
+   app.use(express.favicon());
+   app.use(express.json());
+   app.use(express.urlencoded());
+   app.use(express.methodOverride());
+
+   app.use(app.router);
+   app.use('/public', express.static(path.join(__dirname, 'public')));
+   app.use('/bower', express.static(path.join(__dirname, 'bower_components')));
+});
+
+
+///////////////////////////////////////////////////////////////////////////////
+//                                                                           //
+// Resources                                                                 //
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
+
+app.get('/', function (req, res) { res.render('welcome.html'); });
+app.get('/ping', function (req, res) { res.send('pong'); }); // Sanity check
+
+
+// Server
+var server = http.createServer(app);
+var io = socketio.listen(server);
+io.set('logger', {
+   debug: logger.debug,
+   info: logger.info,
+   warn: logger.warn,
+   error: logger.error
+});
+
+server.listen(app.get('port'), function () {
+   logger.info("Fish server listening on port " + app.get('port'));
+});
---
+++ routes/base.js
@@ -0,0 +1,6 @@
+'use strict';
+
+// GET /
+exports.login = function (req, res) {
+   res.render('login.html');
+}
