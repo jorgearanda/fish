bb4dbc26a4bdece4e18bb07d6d9dee66a8cddd44
Jorge Aranda <jarandag@gmail.com>
2014-02-10 21:50:09 -0800

    More progress on new sim type page

---
+++ public/js/sim-type.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var main = function () {
+   $('#early-end-tooltip').tooltip();
+   $('#max-fish-tooltip').tooltip();
+   $('#available-mystery-tooltip').tooltip();
+   $('#reported-mystery-tooltip').tooltip();
+   $('#spawn-factor-tooltip').tooltip();
+   $('#chance-catch-tooltip').tooltip();
+   $('#show-fisher-status-tooltip').tooltip();
+};
+
+$(document).ready(main);
