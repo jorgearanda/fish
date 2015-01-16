b2d8cd68efa1430b60e8fcbca7f1b25e0ba9b063
Jorge Aranda <jarandag@gmail.com>
2015-01-16 10:33:03 -0800

    Temporarily remove dependencies that require node-gyp compilation. Broken authentication for now

--- models/experimenter-model.js
+++ models/experimenter-model.js
@@ -1,6 +1,5 @@
 'use strict';
 
-var bcrypt = require('bcrypt');
 var mongoose = require('mongoose');
 
 var BCRYPT_COST = 12;
@@ -18,11 +17,15 @@ var experimenterSchema = new Schema({
 });
 
 experimenterSchema.statics.hashPassword = function hash(rawPassword, next) {
-    return bcrypt.hash(rawPassword, BCRYPT_COST, next);
+    // Temporary, to account for bcrypt errors
+    return next(null, rawPassword);
+    //return bcrypt.hash(rawPassword, BCRYPT_COST, next);
 };
 
 experimenterSchema.statics.comparePasswords = function (raw, hash, next) {
-    return bcrypt.compare(raw, hash, next);
+    // Temporary, to account for bcrypt errors
+    return next(null, true);
+    //return bcrypt.compare(raw, hash, next);
 };
 
 exports.Experimenter = mongoose.model('Experimenter', experimenterSchema);
