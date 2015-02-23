be61e00b099e2d91a574bba78f6a74f685c44d47
James-Firth <hello@james-firth.com>
2015-02-22 20:08:14 -0600

    JS - Added functionality to the dropdown - Dropdown only shows static or dynamic radio buttons. - Dropdown shows/hides the appropriate groups - Shows all behaviour options when archived/active
    JADE
    - Switched header to h3 to be the same
    - Using the bootstrap style dropdown
    - Added more classes to make selection of groups and inputs easier
    - Static is the default behaviour shown.

--- public/js/microworld.js
+++ public/js/microworld.js
@@ -13,6 +13,22 @@ function isNewMicroworld() {
     return ($.url().segment(3) === 'new');
 }
 
+function showStatusTableOptions() {
+    var behaviour_name = $(this).attr('id');
+
+    var behaviour_type = $(this).text();
+    $(this).closest('.dropdown').find('span#btn_txt').text(behaviour_type+"  ")
+
+    $("."+behaviour_name).removeClass('hide');
+
+    if(behaviour_name == "static_option"){
+        $(".dynamic_option").addClass('hide');
+    } else {
+        $(".static_option").addClass('hide');
+    }
+
+}
+
 function readyTooltips() {
     $('#early-end-tooltip').tooltip();
     $('#max-fish-tooltip').tooltip();
@@ -505,6 +521,7 @@ function setButtons() {
     $('#activate-confirmed').click(activateMicroworld);
     $('#archive-confirmed').click(archiveMicroworld);
     $('#delete-confirmed').click(deleteMicroworld);
+    $(".behaviour_group_select").click(showStatusTableOptions);
     initDownloadAll();
 }
 
@@ -567,6 +584,9 @@ function prepareControls() {
         $('#delete-2').removeClass('collapse');
         $('.form-control').prop('disabled', 'disabled');
         $('#results').removeClass('collapse');
+        $("#status_table_behaviour").prop('disabled', true);
+        $(".dynamic_option").removeClass("hide");
+        $(".behaviour_input").attr('disabled', true);
     } else if (mode === 'archived') {
         $('title').text('Fish - Archived Microworld');
         $('#microworld-header').text(pageHeader[mode]);
@@ -580,6 +600,9 @@ function prepareControls() {
         $('#delete-2').removeClass('collapse');
         $('.form-control').prop('disabled', 'disabled');
         $('#results').removeClass('collapse');
+        $("#status_table_behaviour").prop('disabled', true);
+        $(".behaviour_input").attr('disabled', true);
+        $(".dynamic_option").removeClass("hide");
     }
 
     uniformityChanges();
