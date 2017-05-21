d6382e4e6dfc852048997c3fd1b8ab092cf2366c
Jorge Aranda <jarandag@gmail.com>
2017-05-20 20:54:49 -0700

    Add Korean translation

--- public/js/localization.js
+++ public/js/localization.js
@@ -7,6 +7,7 @@ var de = {};  // German
 var es = {};  // Spanish
 var fr = {};  // French
 var pt = {};  // Portuguese
+var ko = {};  // Korean
 
 // Costs
 en['costs_fishValue'] = 'Each fish earns you';
@@ -16,6 +17,7 @@ de['costs_fishValue'] = 'Gewinn pro Fisch:';
 es['costs_fishValue'] = 'Cada pez te da';
 fr['costs_fishValue'] = 'Avec chaque poisson vous gagnez';
 pt['costs_fishValue'] = 'Cade peixe rende';
+ko['costs_fishValue'] = '각 물고기당 수입';
 
 en['costs_costCast'] = 'Casting for a fish costs';
 cn['costs_costCast'] = '捕每条鱼的成本是';
@@ -24,6 +26,7 @@ de['costs_costCast'] = 'Kosten pro Angelauswurf:';
 es['costs_costCast'] = 'Pescar un pez cuesta';
 fr['costs_costCast'] = 'Aller pêcher in poisson coûte';
 pt['costs_costCast'] = 'Pescar um peixe custa';
+ko['costs_costCast'] = '물고기 낚시 비용';
 
 en['costs_costSecond'] = 'Each second at sea costs';
 cn['costs_costSecond'] = '每秒在海上的成本是';
@@ -32,6 +35,7 @@ de['costs_costSecond'] = 'Kosten pro Sekunde auf dem Meer:';
 es['costs_costSecond'] = 'Un segundo en el mar cuesta';
 fr['costs_costSecond'] = 'Chaque seconde en mer coûte';
 pt['costs_costSecond'] = 'Cada segundo no mar custa';
+ko['costs_costSecond'] = '초당 해상 비용';
 
 en['costs_costLeave'] = 'Leaving port costs';
 cn['costs_costLeave'] = '出港的费用是';
@@ -40,6 +44,7 @@ de['costs_costLeave'] = 'Kosten zum Verlassen des Hafens';
 es['costs_costLeave'] = 'Salir del puerto cuesta';
 fr['costs_costLeave'] = 'Quitter le port coûte';
 pt['costs_costLeave'] = 'Deixar o porto custa';
+ko['costs_costLeave'] = '출항 비용';
 
 // Status
 en['status_wait'] = 'Please wait';
@@ -49,6 +54,7 @@ de['status_wait'] = 'Bitte warten';
 es['status_wait'] = 'Espera por favor';
 fr['status_wait'] = 'Veuillez patienter';
 pt['status_wait'] = 'Por favor aguarde';
+ko['status_wait'] = '잠시만 기다려주십시오';
 
 en['status_subWait'] = 'Loading the application';
 cn['status_subWait'] = '加载应用程序中';
@@ -57,14 +63,16 @@ de['status_subWait'] = 'Ladevorgang';
 es['status_subwait'] = 'Cargando la aplicación';
 fr['status_subWait'] = 'Chargement de l’application';
 pt['status_subWait'] = 'Carregando a aplicação';
+ko['status_subWait'] = '낚시 게임을 준비하는 중';
 
 en['status_full'] = 'The group you tried to join is full. Please notify the experimenter.';
 cn['status_full'] = '你想加入的组群已满 请告知实验者';
-ct['status_full'] = '你想加入的組群已滿  請告知實驗者';
+ct['status_full'] = '你想加入的組群已滿 請告知實驗者';
 de['status_full'] = 'Die Gruppe, der du beitreten wolltest, ist voll. Bitte benachichtigen Sie den Studienleiter.';
 es['status_full'] = 'El grupo al que trataste de entrar está lleno. Por favor, notifica al experimentador.';
 fr['status_full'] = 'Le groupe que vous tentez de joindre est complet. Merci d’avertir l’expérimentateur.';
 pt['status_full'] = 'O grupo que você tentou entrar esta lotado. Por favor, notifique o experimentador.';
+ko['status_full'] = '참여하고자 하는 그룹이 모두 채워졌습니다. 연구자에게 알려주십시오.';
 
 en['status_getReady'] = 'Get ready! The simulation is about to start.';
 cn['status_getReady'] = '请做好准备！模拟活动即将开始';
@@ -73,6 +81,7 @@ de['status_getReady'] = 'Los geht\'s! Die Simulation wird gestartet.';
 es['status_getReady'] = '¡Prepárate! La simulación está por comenzar.';
 fr['status_getReady'] = 'Soyez prêt ! La simulation va commencer.';
 pt['status_getReady'] = 'Prepare-se! A simulação está prestes a começar.';
+ko['status_getReady'] = '준비하십시오! 게임이 곧 시작됩니다.';
 
 en['status_season'] = 'Season ';
 cn['status_season'] = '季节 ';
@@ -81,6 +90,7 @@ de['status_season'] = 'Saison ';
 es['status_season'] = 'Temporada ';
 fr['status_season'] = 'Saison ';
 pt['status_season'] = 'Temporada ';
+ko['status_season'] = '어획기 ';
 
 en['status_fishBetween'] = 'There are between ';
 cn['status_fishBetween'] = '在…之间 ';
@@ -89,6 +99,7 @@ de['status_fishBetween'] = 'Es gibt zwischen ';
 es['status_fishBetween'] = 'Hay entre ';
 fr['status_fishBetween'] = 'Il y a entre ';
 pt['status_fishBetween'] = 'Existem entre ';
+ko['status_fishBetween'] = '물고기 수 범위 ';
 
 en['status_fishMax'] = 'There are ';
 cn['status_fishMax'] = '在这一刻，有 ';
@@ -97,6 +108,7 @@ de['status_fishMax'] = 'Es gibt ';
 es['status_fishMax'] = 'Hay ';
 fr['status_fishMax'] = 'Il y a ';
 pt['status_fishMax'] = 'Existem ';
+ko['status_fishMax'] = '최대 물고기 수 ';
 
 en['status_fishAnd'] = ' and ';
 cn['status_fishAnd'] = ' 和 ';
@@ -105,6 +117,7 @@ de['status_fishAnd'] = ' und ';
 es['status_fishAnd'] = ' y ';
 fr['status_fishAnd'] = ' et ';
 pt['status_fishAnd'] = ' e ';
+ko['status_fishAnd'] = ' & ';
 
 en['status_fishTo'] = ' to ';
 cn['status_fishTo'] = ' 到 ';
@@ -113,6 +126,7 @@ de['status_fishTo'] = ' bis ';
 es['status_fishTo'] = ' a ';
 fr['status_fishTo'] = ' to ';
 pt['status_fishTo'] = ' a ';
+ko['status_fishTo'] = ' ~ ';
 
 en['status_fishRemaining'] = ' remaining';
 cn['status_fishRemaining'] = ' 剩余';
@@ -121,6 +135,7 @@ de['status_fishRemaining'] = ' uebrig';
 es['status_fishRemaining'] = ' restantes';
 fr['status_fishRemaining'] = ' restant';
 pt['status_fishRemaining'] = ' restantes';
+ko['status_fishRemaining'] = ' 남음';
 
 en['status_spawning'] = 'We are between seasons';
 cn['status_spawning'] = '我们正在两个季节之间';
@@ -129,6 +144,7 @@ de['status_spawning'] = 'Wir sind zwischen den Saisons';
 es['status_spawning'] = 'Estamos entre temporadas';
 fr['status_spawning'] = 'C’est maintenant l’entre-saison';
 pt['status_spawning'] = 'Estamos entre temporadas';
+ko['status_spawning'] = '지금은 어획기가 아닙니다';
 
 en['status_subSpawning'] = 'Fish are now spawning';
 cn['status_subSpawning'] = '鱼正在繁殖';
@@ -137,6 +153,7 @@ de['status_subSpawning'] = 'Die Fische vermehren sich';
 es['status_subSpawning'] = 'Elos peces se están reproduciendo';
 fr['status_subSpawning'] = 'Les poisons se reproduisent';
 pt['status_subSpawning'] = 'Os peixes estão em desova';
+ko['status_subSpawning'] = '산란이 진행 중입니다';
 
 en['status_paused'] = 'Simulation paused.';
 cn['status_paused'] = '模拟活动暂停';
@@ -145,6 +162,7 @@ de['status_paused'] = 'Die Simulation ist pausiert.';
 es['status_paused'] = 'Simulación pausada.';
 fr['status_paused'] = 'Simulation en pause.';
 pt['status_paused'] = 'Simulação pausada.';
+ko['status_paused'] = '게임이 일시정지 되었습니다.';
 
 // Warnings
 en['warning_seasonEnd'] = 'Warning: this season is about to end.';
@@ -154,6 +172,7 @@ de['warning_seasonEnd'] = 'Achtung! Die Saison wird beendet.';
 es['warning_seasonEnd'] = 'Alerta: esta temporada está por terminar.';
 fr['warning_seasonEnd'] = 'Attention, cette saison de pêche va se terminer.';
 pt['warning_seasonEnd'] = 'Aviso: Esta temporada está prestes a acabar.';
+ko['warning_seasonEnd'] = '경고: 이번 어획기가 곧 종료됩니다.';
 
 en['warning_seasonStart'] = 'Get ready: the next season is about to start.';
 cn['warning_seasonStart'] = '准备好：下个季节即将开始';
@@ -162,6 +181,7 @@ de['warning_seasonStart'] = 'Bitte bereit machen! Die naechste Saison beginnt.';
 es['warning_seasonStart'] = '¡Prepárate! La siguiente temporada está por comenzar.';
 fr['warning_seasonStart'] = 'Attention, la prochaine saison de pêche va commencer.';
 pt['warning_seasonStart'] = 'Prepare-se! A próxima temporada está prestes a começar.';
+ko['warning_seasonStart'] = '준비하십시오. 다음 어획기가 곧 시작됩니다.';
 
 // Info
 en['info_you'] = 'You';
@@ -171,6 +191,7 @@ de['info_you'] = 'Sie';
 es['info_you'] = 'Tú';
 fr['info_you'] = 'Vous';
 pt['info_you'] = 'Você';
+ko['info_you'] = '나';
 
 en['info_fishCaught'] = 'Fish caught';
 cn['info_fishCaught'] = '捕捞的鱼';
@@ -179,6 +200,7 @@ de['info_fishCaught'] = 'Gefangene Fische';
 es['info_fishCaught'] = 'Peces';
 fr['info_fishCaught'] = 'Poissons attrapés';
 pt['info_fishCaught'] = 'Peixes pegos';
+ko['info_fishCaught'] = '잡은 물고기';
 
 en['info_profits'] = 'Profits';
 cn['info_profits'] = '利润';
@@ -187,6 +209,7 @@ de['info_profits'] = 'Profit';
 es['info_profits'] = 'Ganancias';
 fr['info_profits'] = 'Benefices';
 pt['info_profits'] = 'Lucros';
+ko['info_profits'] = '수익';
 
 en['info_fisher'] = 'Fisher';
 cn['info_fisher'] = '渔人';
@@ -195,6 +218,7 @@ de['info_fisher'] = 'Fischer';
 es['info_fisher'] = 'Pescador';
 fr['info_fisher'] = 'Pêcheur';
 pt['info_fisher'] = 'Pescador';
+ko['info_fisher'] = '플레이어';
 
 en['info_location'] = 'Location';
 cn['info_location'] = '位置';
@@ -203,6 +227,7 @@ de['info_location'] = 'Aufenthaltsort';
 es['info_location'] = 'Ubicación';
 fr['info_location'] = 'Lieu';
 pt['info_location'] = 'Local';
+ko['info_location'] = '현재 위치';
 
 en['info_season'] = 'Season';
 cn['info_season'] = '季节';
@@ -211,6 +236,7 @@ de['info_season'] = 'Saison';
 es['info_season'] = 'Temporada';
 fr['info_season'] = 'Saison';
 pt['info_season'] = 'Temporada';
+ko['info_season'] = '어획기';
 
 en['info_overall'] = 'Overall';
 cn['info_overall'] = '总体';
@@ -219,6 +245,7 @@ de['info_overall'] = 'Gesamt';
 es['info_overall'] = 'Total';
 fr['info_overall'] = 'En tout';
 pt['info_overall'] = 'Total';
+ko['info_overall'] = '총';
 
 // End report
 en['end_over'] = 'This simulation is over.';
@@ -228,6 +255,7 @@ de['end_over'] = 'Die Simulation ist nun beendet.';
 es['end_over'] = 'Esta simulación ha terminado.';
 fr['end_over'] = 'La simulation est terminée.';
 pt['end_over'] = 'A simulação acabou.';
+ko['end_over'] = '게임이 모두 종료되었습니다.';
 
 en['end_caught'] = 'You caught';
 cn['end_caught'] = '你捕捞了';
@@ -236,6 +264,7 @@ de['end_caught'] = 'Sie haben';
 es['end_caught'] = 'Pescaste';
 fr['end_caught'] = 'Vous avez pêché';
 pt['end_caught'] = 'Você pescou';
+ko['end_caught'] = '내가 잡은';
 
 en['end_fish'] = 'fish.';
 cn['end_fish'] = '鱼';
@@ -244,6 +273,7 @@ de['end_fish'] = 'Fisch(e) gefangen.';
 es['end_fish'] = 'peces.';
 fr['end_fish'] = 'poisson.';
 pt['end_fish'] = 'peixes.';
+ko['end_fish'] = '물고기.';
 
 en['end_money'] = 'You ended with';
 cn['end_money'] = '你获得';
@@ -252,6 +282,7 @@ de['end_money'] = 'Ihre Gewinn:';
 es['end_money'] = 'Terminaste con';
 fr['end_money'] = 'Vous terminez avec';
 pt['end_money'] = 'Você terminou com';
+ko['end_money'] = '최종 수입';
 
 // Buttons
 en['buttons_goToSea'] = 'Go to sea <i class="icon-ship"></i>';
@@ -261,6 +292,7 @@ de['buttons_goToSea'] = 'Aufs Meer fahren <i class="icon-ship"></i>';
 es['buttons_goToSea'] = 'Ir al mar <i class="icon-ship"></i>';
 fr['buttons_goToSea'] = 'Allez en mer <i class="icon-ship"></i>';
 pt['buttons_goToSea'] = 'Ir para o mar <i class="icon-ship"></i>';
+ko['buttons_goToSea'] = '바다로 나가기 <i class="icon-ship"></i>';
 
 en['buttons_castFish'] = 'Attempt to fish <i class="icon-fish-hook"></i>';
 cn['buttons_castFish'] = '尝试捕鱼 <i class="icon-fish-hook"></i>';
@@ -269,6 +301,7 @@ de['buttons_castFish'] = 'Fisch angeln <i class="icon-fish-hook"></i>';
 es['buttons_castFish'] = 'Pescar un pez <i class="icon-fish-hook"></i>';
 fr['buttons_castFish'] = 'Essayez de pêcher <i class="icon-fish-hook"></i>';
 pt['buttons_castFish'] = 'Pescar um peixe <i class="icon-fish-hook"></i>';
+ko['buttons_castFish'] = '낚시하기 <i class="icon-fish-hook"></i>';
 
 en['buttons_return'] = 'Return to port <i class="icon-lighthouse"></i>';
 cn['buttons_return'] = '返航 <i class="icon-lighthouse"></i>';
@@ -277,6 +310,7 @@ de['buttons_return'] = 'Zum Hafen zurück <i class="icon-lighthouse"></i>';
 es['buttons_return'] = 'Regresar al puerto <i class="icon-lighthouse"></i>';
 fr['buttons_return'] = 'Retournez au port <i class="icon-lighthouse"></i>';
 pt['buttons_return'] = 'Retornar ao porto <i class="icon-lighthouse"></i>';
+ko['buttons_return'] = '항구로 돌아가기 <i class="icon-lighthouse"></i>';
 
 en['buttons_pause'] = 'Pause <i class="icon-pause"></i>';
 cn['buttons_pause'] = '暂停 <i class="icon-pause"></i>';
@@ -285,6 +319,7 @@ de['buttons_pause'] = 'Pause <i class="icon-pause"></i>';
 es['buttons_pause'] = 'Pausar <i class="icon-pause"></i>';
 fr['buttons_pause'] = 'Attendez <i class="icon-pause"></i>';
 pt['buttons_pause'] = 'Parar <i class="icon-pause"></i>';
+ko['buttons_pause'] = '일시정지 <i class="icon-pause"></i>';
 
 en['buttons_resume'] = 'Resume <i class="icon-play"></i>';
 cn['buttons_resume'] = '继续 <i class="icon-play"></i>';
@@ -293,6 +328,7 @@ de['buttons_resume'] = 'Fortfahren <i class="icon-play"></i>';
 es['buttons_resume'] = 'Continuar <i class="icon-play"></i>';
 fr['buttons_resume'] = 'Recommencez <i class="icon-play"></i>';
 pt['buttons_resume'] = 'Retornar <i class="icon-play"></i>';
+ko['buttons_resume'] = '다시 시작 <i class="icon-play"></i>';
 
 en['buttons_goFishing'] = 'Go fishing!';
 cn['buttons_goFishing'] = '去捕鱼';
@@ -301,6 +337,7 @@ de['buttons_goFishing'] = 'Angeln fahren';
 es['buttons_goFishing'] = '¡A pescar!';
 fr['buttons_goFishing'] = 'Allez pêcher';
 pt['buttons_goFishing'] = 'Ir pescar!';
+ko['buttons_goFishing'] = '낚시하러 가기';
 
 // Login
 en['login_title'] = 'Welcome to FISH!';
@@ -310,6 +347,7 @@ de['login_title'] = 'Wilkommen bei FISH!';
 es['login_title'] = '¡Bienvenidos a FISH!';
 fr['login_title'] = 'Bienvenu sur FISH!';
 pt['login_title'] = 'Bem vindo ao FISH!';
+ko['login_title'] = 'FISH 게임에 접속하신 것을 환영합니다!';
 
 en['login_invalidGroup'] = 'Group name invalid. Please try again.';
 cn['login_invalidGroup'] = '组名无效。请再试一次';
@@ -318,6 +356,7 @@ de['login_invalidGroup'] = 'Gruppenname ungueltig. Bitte versuchen Sie es erneut
 es['login_invalidGroup'] = 'Nombre de grupo inválido. Por favor, intenta de nuevo.';
 fr['login_invalidGroup'] = 'Nom du groupe non valable. Essayez de nouveau.';
 pt['login_invalidGroup'] = 'Nome de grupo invalido. Por favor, tente de novo.';
+ko['login_invalidGroup'] = '사용할 수 없는 그룹명입니다. 다시 시도해주십시오.';
 
 en['login_validating'] = 'Validating';
 cn['login_validating'] = '证实中';
@@ -326,6 +365,7 @@ de['login_validating'] = 'Login wird ueberprueft.';
 es['login_validating'] = 'Validando';
 fr['login_validating'] = 'Validation';
 pt['login_validating'] = 'Validando';
+en['login_validating'] = '확인 중';
 
 en['login_welcome'] = 'Welcome to FISH, and thanks for participating!';
 cn['login_welcome'] = '欢迎到来，感谢您的参与';
@@ -334,6 +374,7 @@ de['login_welcome'] = 'Wilkommen bei FISH und danke fuer Ihre Teilnahme!';
 es['login_welcome'] = '¡Bienvenido(a) a FISH, y gracias por participar!';
 fr['login_welcome'] = 'Bienvenu a FISH, et merci de votre participation';
 pt['login_welcome'] = 'Bem vindo ao FISH, e obrigado por participar!';
+ko['login_welcome'] = 'FISH 게임에 접속하신 것을 환영합니다! 게임에 참여해 주셔서 감사합니다.';
 
 en['login_instructions'] = 'Enter the name of your group and your participant ID.';
 cn['login_instructions'] = '请输入您的组群名称和您的参与者ID';
@@ -342,6 +383,7 @@ de['login_instructions'] = 'Bitte geben Sie den Gruppennamen und die Teilnehmern
 es['login_instructions'] = 'Cuando estés preparado(a), escribe el nombre de tu grupo, y tu identificación de participante.';
 fr['login_instructions'] = 'Entrez le nom de votre groupe et de code de participant.';
 pt['login_instructions'] = 'Quando você estiver pronto, entre o nome do seu grupo, e sua identificação de participante.';
+ko['login_instructions'] = '그룹명과 ID를 입력해주십시오.';
 
 en['login_simulationName'] = 'Experiment code';
 cn['login_simulationName'] = '实验代码';
@@ -350,6 +392,7 @@ de['login_simulationName'] = 'Experiment Code';
 es['login_simulationName'] = 'Código del experimento';
 fr['login_simulationName'] = 'Code expérimental';
 pt['login_simulationName'] = 'Código da experiência';
+ko['login_simulationName'] = '실험 코드';
 
 en['login_participantId'] = 'Participant ID';
 cn['login_participantId'] = '参与者ID';
@@ -358,6 +401,7 @@ de['login_participantId'] = 'Teilnehmernummer';
 es['login_participantId'] = 'Identificación del participante';
 fr['login_participantId'] = 'Code du participant';
 pt['login_participantId'] = 'Identificação do participante';
+ko['login_participantId'] = '참여자 ID';
 
 en['login_getStarted'] = 'Get Started!';
 cn['login_getStarted'] = '准备开始';
@@ -366,6 +410,7 @@ de['login_getStarted'] = 'Los geht\'s!';
 es['login_getStarted'] = '¡Comenzar!';
 fr['login_getStarted'] = 'Commencez';
 pt['login_getStarted'] = 'Começar!';
+ko['login_getStarted'] = '시작하기';
 
 langs['en'] = en;
 langs['cn'] = cn;
@@ -374,3 +419,4 @@ langs['de'] = de;
 langs['es'] = es;
 langs['fr'] = fr;
 langs['pt'] = pt;
+langs['ko'] = ko;
