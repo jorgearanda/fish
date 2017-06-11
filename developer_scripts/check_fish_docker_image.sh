a991c5d51654133e553c5c104ed62dfba97fa836
btara.truhandarien <btara.truhandarien@rakuten.com>
2017-06-11 21:56:24 +0900

    Deleted uneeded scripts

--- developer_scripts/check_fish_docker_image.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-FULL_FILENAME=$(realpath $0)
-SCRIPT_BASEDIR=$(dirname $FULL_FILENAME)
-echo "Checking for docker image 'fishsim'..."
-docker image inspect fishsim 1> /dev/null
-if [ $? -ne 0 ]; then
-	docker build -t fishsim $(dirname $SCRIPT_BASEDIR)/Dockerfile-App
-fi
