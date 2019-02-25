9b667107a6a9f1f5c29f7fd617fe65ace484df5f
Jorge Aranda <jorge.aranda@cuevano.ca>
2019-02-24 21:49:21 -0800

    Prettify client-side profile JS

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,52 +1,53 @@
 'use strict';
-/*global alert:true*/
+/*global alert:true, $:true*/
 
 function submitUpdate() {
-    var name = $('#name').val();
-    var email = $('#email').val();
-    var password = $('#password').val();
-    var confirmPass = $('#confirmPass').val();
+  var name = $('#name').val();
+  var email = $('#email').val();
+  var password = $('#password').val();
+  var confirmPass = $('#confirmPass').val();
 
-    var sendData = {};
-    if (name.length >= 1) sendData.name = name;
-    if (email.length >= 1) sendData.email = email;
-    if (password.length >= 1) sendData.rawPassword = password;
-    if (confirmPass.length >= 1) sendData.confirmPass = confirmPass;
+  var sendData = {};
+  if (name.length >= 1) sendData.name = name;
+  if (email.length >= 1) sendData.email = email;
+  if (password.length >= 1) sendData.rawPassword = password;
+  if (confirmPass.length >= 1) sendData.confirmPass = confirmPass;
 
-    var expId = location.pathname.split('/')[2];
-    $.ajax({
-        type: 'PUT',
-        url: '/experimenters/' + expId,
-        data: sendData,
-        success: function() {
-            location.replace('./dashboard');
-        },
-        error: function(jqXHR) {
-            if (jqXHR.status === 409) {
-                if (jqXHR.responseText === 'password conflict') {
-                    alert('The passwords given did not match');
-                } else {
-                    alert('Invalid email');
-                }
-            } else if (jqXHR.status === 403) {
-                alert('At least one field needs to be filled');
-            } else if (jqXHR.status === 401) {
-                alert('You are unauthorized to change this profile');
-            } else if (jqXHR.status === 400) {
-                alert('Did not match any experimenters');
-            } else { // status 500
-                alert('Internal error');
-            }
+  var expId = location.pathname.split('/')[2];
+  $.ajax({
+    type: 'PUT',
+    url: '/experimenters/' + expId,
+    data: sendData,
+    success: function() {
+      location.replace('./dashboard');
+    },
+    error: function(jqXHR) {
+      if (jqXHR.status === 409) {
+        if (jqXHR.responseText === 'password conflict') {
+          alert('The passwords given did not match');
+        } else {
+          alert('Invalid email');
         }
-    });
+      } else if (jqXHR.status === 403) {
+        alert('At least one field needs to be filled');
+      } else if (jqXHR.status === 401) {
+        alert('You are unauthorized to change this profile');
+      } else if (jqXHR.status === 400) {
+        alert('Did not match any experimenters');
+      } else {
+        // status 500
+        alert('Internal error');
+      }
+    },
+  });
 }
 
 function setButtons() {
-    $('#do-update').click(submitUpdate);
+  $('#do-update').click(submitUpdate);
 }
 
 function main() {
-    setButtons();
+  setButtons();
 }
 
 $(document).ready(main);
