a81fd73d6d6e2e6858c890f4436ffc1b2b65bdc3
Hans <koomen@cs.rochester.edu>
2024-03-27 11:50:27 -0400

    Always include run id, even when downloading 1 run

--- src/routes/runs.js
+++ src/routes/runs.js
@@ -24,7 +24,8 @@ function flattenRunResults(runs) {
 
       for (var k = 0; k < fishers.length; k++) {
         var toPush = {};
-        if (produceAllRuns) toPush['Run ID'] = parentalId;
+        // if (produceAllRuns) 
+        toPush['Run ID'] = parentalId;
         toPush.Fisher = fishers[k].name;
         toPush.Type = fishers[k].type;
         toPush.Greed = fishers[k].greed;
